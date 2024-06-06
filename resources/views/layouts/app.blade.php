eaf63d17fddbdacae5be39051e3174a272591b64
Rafly Maulidan <cyberschoolindo@gmail.com>
2024-06-06 18:37:44 +0700

    Mengubah title & icon web

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,13 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Praktek Kerja Lapangan</title>
-
-    <!-- Favicon -->
-    <link rel="website icon" type="png" href="{{ asset('img/logo-sekolah.png') }}">
+    <title>SI PKL</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
+
+    <!-- Favicon -->
+    <link rel="website icon" type="png" href="{{ asset('img/logo-sekolah.png') }}">
 
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
