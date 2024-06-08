8594df56f63a14e6c25026a0428410b193008a1e
Rafly Maulidan <cyberschoolindo@gmail.com>
2024-06-09 00:03:06 +0700

    Membuat responsif tampilan login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,3 +1,34 @@
+{{-- <x-guest-layout>
+    <!-- Session Status -->
+    <x-auth-session-status class="mb-4" :status="session('status')" />
+
+    <form method="POST" action="{{ route('login') }}">
+        @csrf
+
+        <!-- Username -->
+        <div>
+            <x-input-label for="username" :value="__('Username')" />
+            <x-text-input id="username" class="block mt-1 w-full" type="text" name="username" :value="old('username')" required
+                autofocus autocomplete="username" placeholder="Masukan NIS atau Email..." />
+            <x-input-error :messages="$errors->get('username')" class="mt-2" />
+        </div>
+
+        <!-- Password -->
+        <div class="mt-4">
+            <x-input-label for="password" :value="__('Password')" />
+            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                autocomplete="current-password" placeholder="Masukan Password..." />
+            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+        </div>
+
+        <!-- Button -->
+        <div class="flex items-center justify-end mt-6">
+            <button type="submit" class="btn btn-danger w-full sm:w-auto rounded-lg">
+                {{ __('Masuk') }}
+            </button>
+        </div>
+    </form>
+</x-guest-layout> --}}
 <x-guest-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
@@ -8,22 +39,24 @@
         <!-- Username -->
         <div>
             <x-input-label for="username" :value="__('Username')" />
-            <x-text-input id="username" class="block mt-1 w-full" type="text" name="username" :value="old('username')" required autofocus autocomplete="username" />
+            <x-text-input id="username" class="block mt-1 w-full rounded-3xl" type="text" name="username"
+                :value="old('username')" required autofocus autocomplete="username" placeholder="Masukan NIS atau Email..." />
             <x-input-error :messages="$errors->get('username')" class="mt-2" />
         </div>
 
         <!-- Password -->
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
-            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+            <x-text-input id="password" class="block mt-1 w-full rounded-3xl" type="password" name="password" required
+                autocomplete="current-password" placeholder="Masukan Password..." />
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
+        <!-- Button -->
+        <div class="flex items-center justify-end mt-10">
+            <button type="submit" class="btn btn-danger w-full sm:w-24 rounded-3xl">
+                {{ __('Masuk') }}
+            </button>
         </div>
     </form>
-
 </x-guest-layout>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,33 +1,56 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>SI PKL</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        {{-- icon --}}
-        <link rel="icon" type="png" href="{{ asset('img/logo-sekolah.png') }}">
+    <title>SI PKL</title>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    {{-- icon --}}
+    <link rel="icon" type="png" href="{{ asset('img/logo-sekolah.png') }}">
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    {{-- bootstrap --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+    </script>
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="font-sans text-gray-900 antialiased">
+    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+        <div class="flex flex-col sm:flex-row items-center">
+            <div class="flex flex-col items-center mr-10">
+                <img src="{{ asset('img/logo-jurusan.png') }}" alt="Logo Jurusan"
+                    class="mb-4 hidden sm:block w-auto sm:h-52">
+                <img src="{{ asset('img/logo-sekolah.png') }}" alt="Logo Sekolah"
+                    class="hidden sm:block w-auto rounded-full sm:h-52">
             </div>
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+            <div class="w-96 sm:max-w-md px-12 py-12 bg-white shadow-md overflow-hidden sm:rounded-2xl"
+                style="min-height: 430px;">
+                <div class="flex">
+                    <img src="{{ asset('img/logo-jurusan.png') }}" alt="Logo Jurusan"
+                        class="mb-4 h-14 w-1h-14 sm:hidden">
+                    <div class="text-center px-1">
+                        <h1 class="text-sm sm:text-xl font-bold pb-10">SISTEM INFORMASI PRAKTEK KERJA LAPANGAN</h1>
+                    </div>
+                    <img src="{{ asset('img/logo-sekolah.png') }}" alt="Logo Sekolah"
+                        class="h-14 w-1h-14 rounded-full sm:hidden">
+                </div>
                 {{ $slot }}
             </div>
         </div>
-    </body>
+    </div>
+</body>
+
 </html>
