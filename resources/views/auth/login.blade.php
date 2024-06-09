ade6ad8b7867f9b15bb68961555cfeb1a955cb09
Rafly Maulidan <cyberschoolindo@gmail.com>
2024-06-09 14:49:39 +0700

    Fix - Form input login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -39,7 +39,7 @@
         <!-- Username -->
         <div>
             <x-input-label for="username" :value="__('Username')" />
-            <x-text-input id="username" class="block mt-1 w-full rounded-3xl" type="text" name="username"
+            <x-text-input id="username" style="border-radius: 24px;" class="block mt-1 w-full sm:rounded-3xl" type="text" name="username"
                 :value="old('username')" required autofocus autocomplete="username" placeholder="Masukan NIS atau Email..." />
             <x-input-error :messages="$errors->get('username')" class="mt-2" />
         </div>
@@ -47,7 +47,7 @@
         <!-- Password -->
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
-            <x-text-input id="password" class="block mt-1 w-full rounded-3xl" type="password" name="password" required
+            <x-text-input id="password" style="border-radius: 24px;" class="block mt-1 w-full sm:rounded-3xl" type="password" name="password" required
                 autocomplete="current-password" placeholder="Masukan Password..." />
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
