df93225059fdc71f9cd80c80b71c7525d125997a
“Rafly <cyberschoolindo@gmail.com>
2024-06-23 18:12:09 +0700

    Finishing tampilan form login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,14 +8,14 @@
         <!-- Username -->
         <div>
             <x-input-label for="username" :value="__('Username')" />
-            <x-text-input id="username" style="border-radius: 24px;" class="block mt-1 w-full sm:rounded-3xl" type="text" name="username" :value="old('username')" required autofocus autocomplete="username" placeholder="Masukan NIS atau Email..." />
+            <x-text-input id="username" style="border-radius: 24px;" class="block pl-4 mt-1 w-full sm:rounded-3xl" type="text" name="username" :value="old('username')" required autofocus autocomplete="off" placeholder="Masukan NIS atau Email..." />
             <x-input-error :messages="$errors->get('username')" class="mt-2" />
         </div>
 
         <!-- Password -->
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
-            <x-text-input id="password" style="border-radius: 24px;" class="block mt-1 w-full sm:rounded-3xl" type="password" name="password" required autocomplete="current-password" placeholder="Masukan Password..." />
+            <x-text-input id="password" style="border-radius: 24px;" class="block pl-4 mt-1 w-full sm:rounded-3xl" type="password" name="password" required autocomplete="current-password" placeholder="Masukan Password..." />
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
